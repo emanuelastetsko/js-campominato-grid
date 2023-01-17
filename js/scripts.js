46e1c0ae112b9eb02cc1fc61e1bb253e184b155b
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-01-17 16:53:56 +0100

    Pulsante play + colore alle celle cliccate

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,46 @@
+const container = document.getElementById("container");
+
+container.style.display = "none";
+
+
 const grid = document.getElementById("grid");
 
-// Generazione celle della griglia
-for (let index = 0; index < 100; index++) {
-    grid.innerHTML += `<div class=square><div>`;
+// Ciclo per generazione delle celle 
+for (let index = 1; index <= 100; index++) {
+    
+    // Creazione cella 
+    const cell = document.createElement("div");
+    cell.classList.add("square");
+    grid.append(cell);
+
+    //Al click colorare le celle di rosa 
+    cell.addEventListener("click",
+        function () {
+            console.log("Hai cliccato la cella n." + index);
+            
+            if(cell.classList.contains("click")){
+                cell.classList.remove("click");
+            }
+            else{
+                cell.classList.add("click");
+            }
+
+        }
+    );
+
+    
 }
+
+
+const myButton = document.getElementById("play");
+
+// Al click deve apparire la griglia con tutte le sue celle
+myButton.addEventListener("click",
+  function () {
+
+    container.style.display = "inline";
+    console.log("Il gioco è iniziato");
+    
+  }
+
+);
