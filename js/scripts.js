99c9e1382ed7303943ed8be8fe97195b70ea13a8
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-01-18 11:26:21 +0100

    Sistemata funzione generazione tabella

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,45 @@ container.style.display = "none";
 
 const grid = document.getElementById("grid");
 
-// Ciclo per generazione delle celle 
-for (let index = 1; index <= 100; index++) {
-    
-    // Creazione cella 
-    const cell = document.createElement("div");
-    cell.classList.add("square");
-    grid.append(cell);
 
-    //Al click colorare le celle di rosa 
-    cell.addEventListener("click",
-        function () {
-            console.log("Hai cliccato la cella n." + index);
-            
-            if(cell.classList.contains("click")){
-                cell.classList.remove("click");
+function generaTabella() {
+  // Ciclo per generazione delle celle 
+  
+    for (let index = 1; index <= 100; index++) {
+        
+        // Creazione cella 
+        const cell = document.createElement("div");
+        cell.classList.add("square");
+        grid.append(cell);
+        
+
+        //Al click colorare le celle di rosa 
+        cell.addEventListener("click",
+            function () {
+                console.log("Hai cliccato la cella n." + index);
+                
+                if(cell.classList.contains("click")){
+                    cell.classList.remove("click");
+                }
+                else{
+                    cell.classList.add("click");
+                }
+
             }
-            else{
-                cell.classList.add("click");
-            }
+        );
 
-        }
-    );
+        
+    }
 
-    
 }
+
+
+function svuotaTabella() {
+    // Svuoto la tabella
+    grid.innerHTML = "";
+    console.log("La tabella è stata svuotata");
+}
+
 
 
 const myButton = document.getElementById("play");
@@ -38,7 +52,10 @@ const myButton = document.getElementById("play");
 myButton.addEventListener("click",
 
   function () {
-
+    svuotaTabella();
+    generaTabella();
+    
+      
     container.style.display = "inline";
     console.log("Il gioco è iniziato");
     
